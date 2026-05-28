8714d0352393088c1cf974c8febafcdc4827942f
mytools <sinadenis50@gmail.com>
2026-05-28 13:16:02 +0200

    Supabase connected and auth setup

--- app/supabase.ts
+++ app/supabase.ts
@@ -3,16 +3,21 @@ import "react-native-url-polyfill/auto";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://sfdiavixrtzeldstlwfy.supabase.co";
+const supabaseUrl =
+  "https://sdfvialxrfzeldstlwfy.supabase.co";
 
 const supabaseAnonKey =
   "sb_publishable_c_XY1bp5K67R7XTMl3d-fA_HKdR8X4m";
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
-  auth: {
-    storage: AsyncStorage,
-    autoRefreshToken: true,
-    persistSession: true,
-    detectSessionInUrl: false,
-  },
-});
+export const supabase = createClient(
+  supabaseUrl,
+  supabaseAnonKey,
+  {
+    auth: {
+      storage: AsyncStorage,
+      autoRefreshToken: true,
+      persistSession: true,
+      detectSessionInUrl: false,
+    },
+  }
+);
